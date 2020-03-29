55fa4308ffd3f037c9d886d6cc503cf3b471db72
unknown <dethgrenade@gmail.com>
2020-03-29 11:03:16 -0700

    added bookmarks menu to navbar on pages with a bookmarks aside menu

--- assets/scripts/nav-menus.js
+++ assets/scripts/nav-menus.js
@@ -1,14 +1,39 @@
-//Nav Btn Menu Bars
+//Nav Btn, Menu Bars and Links
+const mobileNavBtn = document.getElementById("mobile-nav-btn")
 const barOne = document.getElementById("bar-one");
 const barTwo = document.getElementById("bar-two");
 const barThree = document.getElementById("bar-three");
 const navLinks = document.getElementsByClassName("nav-links");
 
+//Bookmarks Btn and Bookmarks menu
+const bookmarksBtn = document.getElementById("bookmarks-btn");
+const sectionBookmarks = document.getElementsByClassName("section-bookmarks");
 
-//Nav Btn animation
-const mobileNavBtn = document.getElementById("mobile-nav-btn").addEventListener("click", () => {
+
+//Nav Btn Animation
+mobileNavBtn.addEventListener("click", () => {
+    //Open the bookmarks menu
     barOne.classList.toggle("change");
     barTwo.classList.toggle("change");
     barThree.classList.toggle("change");
     navLinks[0].classList.toggle("change");
+
+    //Close bookmarks if open
+    if(bookmarksBtn){
+        sectionBookmarks[0].classList.remove("change");
+    }
 });
+
+//Guide Bookmarks Animation
+if(bookmarksBtn){
+    bookmarksBtn.addEventListener("click", () => {
+        //Open the bookmarks menu
+        sectionBookmarks[0].classList.toggle("change");
+    
+        //Close Nav menu if open
+        barOne.classList.remove("change");
+        barTwo.classList.remove("change");
+        barThree.classList.remove("change");
+        navLinks[0].classList.remove("change");
+    });
+}
