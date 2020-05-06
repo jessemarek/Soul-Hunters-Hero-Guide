efdfc6a76f9876d88a48430a048fbf74ea9ac27e
unknown <dethgrenade@gmail.com>
2020-05-06 08:21:03 -0700

    possible fix to Apple cookie bug

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -92,7 +92,7 @@ for(let i = 0; i < settingsCheckbox.length; i++) {
 function setCookie(cname, cvalue) {
     //Create the expiration date
     const date = new Date();
-    date.setTime(date.getTime() + (30*24*60*60*1000));
+    date.setTime(date.getTime() + (7*24*60*60*1000));
     const expires = `expires=${date.toUTCString()};`;
 
     //Sets the cookie value and expiration
